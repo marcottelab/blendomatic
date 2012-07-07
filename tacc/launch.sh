dff25dbce9428ec8d7472343269d893eb3d59888
Blake Borgeson <git@blakeb.org>
2012-07-07 14:53:17 -0500

    Automate customizing name, hours, type for launcher jobs

---
+++ tacc/launch.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Chiefly for inspect runs since they're slow and need to be parallelized through Launcher
+usage="usage: launch.sh <jobname> <numhours> <qtype{long|normal}>"
+args=("$@")
+if [ ${#args[@]} -lt 3 ]; then
+    echo $usage
+    exit 1
+fi
+name=${args[0]}
+hours=${args[1]}
+qtype=${args[2]}
+submit_script='tacc/scripts/submit/launcher.sge'
+cp tacc/scripts/launcher_head.sge $submit_script
+echo '#$ -N '$name >> $submit_script
+echo '#$ -l h_rt='${hours}':00:00' >> $submit_script
+echo '#$ -q '${qtype} >> $submit_script #long
+cat tacc/scripts/launcher_body.sge >> $submit_script
+
+#qsub < $submit_script
+cat $submit_script
