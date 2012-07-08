03a8d855b5eeb7f1e2eb44adc3415be8a18c78bf
Blake Borgeson <git@blakeb.org>
2012-07-07 19:33:49 -0500

    Scripts to 1)launch inspect with launcher and 2)check search status.

---
+++ tacc/quicklaunch.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+usage="usage: quicklaunch.sh <foldermatch>"
+if [ $# -lt 1 ]; then
+    echo $usage
+    exit 1
+fi
+match=$1
+
+for f in $(ls -d ${match}*/)
+do 
+    f=${f%?} 
+    name=i${f:0:1}${f: -6}
+    plist=tacc/scripts/submit/paramlist.$name
+    rm $plist
+    echo "~/git/blendomatic/scripts/main.sh $f mzXML/$f ${f:0:2}_longest 0.01 inspect 1" >> $plist
+    for i in $(seq 7)
+    do 
+        echo "sleep 180; $f/scripts/run-inspect.sh" >> $plist
+    done
+    ./tacc/launch.sh $name 1 normal
+done
---
+++ tacc/status.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+usage="usage: status.sh <search> <sp>"
+if [ $# -lt 1 ]; then
+    echo $usage
+    exit 1
+fi
+search=$1
+sp=$2
+
+echo "*mzXML *in *out *tmp"
+for f in $(ls -d ${sp}*/); do echo $f $(ls $f/mzXML/*mzXML | wc -l) $(ls $f/$search/*in 2>/dev/null | wc -l) $(ls $f/$search/*out 2> /dev/null | wc -l) $(ls $f/$search/*tmp 2> /dev/null | wc -l); done
