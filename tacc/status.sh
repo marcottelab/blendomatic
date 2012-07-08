1cc1c527812c703f62ea977a5f1eaa18b83f37f6
Blake Borgeson <git@blakeb.org>
2012-07-08 09:25:27 -0500

    status also check *best output

--- tacc/status.sh
+++ tacc/status.sh
@@ -7,5 +7,5 @@ fi
 search=$1
 sp=$2
 
-echo "*mzXML *in *out *tmp"
-for f in $(ls -d ${sp}*/); do echo $f $(ls $f/mzXML/*mzXML | wc -l) $(ls $f/$search/*in 2>/dev/null | wc -l) $(ls $f/$search/*out 2> /dev/null | wc -l) $(ls $f/$search/*tmp 2> /dev/null | wc -l); done
+echo "*mzXML *in *out *tmp *best"
+for f in $(ls -d ${sp}*/); do echo $f $(ls $f/mzXML/*mzXML | wc -l) $(ls $f/$search/*in 2>/dev/null | wc -l) $(ls $f/$search/*out 2> /dev/null | wc -l) $(ls $f/$search/*tmp 2> /dev/null | wc -l) $(ls $f/$search/*best 2> /dev/null | wc -l); done
