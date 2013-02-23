db15eefe165bee7924fab9e81c5f620c1b8050ba
Blake Borgeson <git@blakeb.org>
2013-02-23 10:23:45 -0600

    stampede edit

--- tacc/run_stampede_template.sh
+++ tacc/run_stampede_template.sh
@@ -11,7 +11,7 @@ name=${args[1]}
 hours=${args[2]}
 tasks=${args[3]}
 submit_script='tacc/scripts/submit/template_submit.sh'
-cp tacc/scripts/template.sh $submit_script
+cp tacc/scripts/stampede_template.sh $submit_script
 echo '#SBATCH -J '$name >> $submit_script
 echo '#SBATCH -t '$hours':00:00' >> $submit_script
 echo '#SBATCH -n '$tasks >> $submit_script
