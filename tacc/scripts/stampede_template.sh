5c9b0c77b94119f9739eb429f0fa69e7bf35836b
Blake Borgeson <git@blakeb.org>
2013-02-23 09:38:35 -0600

    stampede submit edits

--- tacc/scripts/stampede_template.sh
+++ tacc/scripts/stampede_template.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#$ -o $SLURM_JOB_NAME.o$SLURM_JOB_ID
-#$ --mail-user=borgeson@utexas.edu
-#$ --mail-type=END                # Email at End of job
-#$ -p normal     # on stampede always normal queue
+#SBATCH -o $SLURM_JOB_NAME.o$SLURM_JOB_ID
+#SBATCH --mail-user=borgeson@utexas.edu
+#SBATCH --mail-type=END                # Email at End of job
+#SBATCH -p normal     # on stampede always normal queue
 set -x                  # Echo commands, use "set echo" with csh
 
