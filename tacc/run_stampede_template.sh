e08c5d71281ba8719aff12df2deb50a914055ca6
Blake Borgeson <git@blakeb.org>
2013-02-23 10:17:52 -0600

    stampede edit

--- tacc/run_stampede_template.sh
+++ tacc/run_stampede_template.sh
@@ -2,14 +2,14 @@
 usage="usage: run_template_command.sh <command> <jobname> <numhours> 
 <numTasks>"
 args=("$@")
-if [ ${#args[@]} -lt 5 ]; then
+if [ ${#args[@]} -lt 4 ]; then
     echo $usage
     exit 1
 fi
 command=${args[0]}
 name=${args[1]}
 hours=${args[2]}
-tasks=${args[3]/_/ }
+tasks=${args[3]}
 submit_script='tacc/scripts/submit/template_submit.sh'
 cp tacc/scripts/template.sh $submit_script
 echo '#SBATCH -J '$name >> $submit_script
