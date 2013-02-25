6f7ec57eef45918160765e3308d6bf4a66f4100b
Blake Borgeson <git@blakeb.org>
2013-02-25 12:54:56 -0600

    load python in script

--- tacc/run_stampede_template.sh
+++ tacc/run_stampede_template.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-usage="usage: run_template_command.sh <command> <jobname> <numhours> 
-<numTasks>"
+usage="usage: run_template_command.sh <command> <jobname> <numhours> <numTasks>"
 args=("$@")
 if [ ${#args[@]} -lt 4 ]; then
     echo $usage
@@ -11,6 +10,7 @@ name=${args[1]}
 hours=${args[2]}
 tasks=${args[3]}
 submit_script='tacc/scripts/submit/template_submit.sh'
+module load python
 cp tacc/scripts/stampede_template.sh $submit_script
 echo '#SBATCH -J '$name >> $submit_script
 echo '#SBATCH -t '$hours':00:00' >> $submit_script
